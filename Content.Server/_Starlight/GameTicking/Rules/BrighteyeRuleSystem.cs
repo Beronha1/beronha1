65ee823f9b73d12f9aeefd2a147abae6e2b441f7
Whiskey Station <311930406+Whiskey-Station@users.noreply.github.com>
2026-08-25 11:48:54 -0400

    Corrige nascimento do Bright-eye na Escuridão

--- Content.Server/_Starlight/GameTicking/Rules/BrighteyeRuleSystem.cs
+++ Content.Server/_Starlight/GameTicking/Rules/BrighteyeRuleSystem.cs
@@ -5,9 +5,13 @@
 // Portado de https://github.com/ss14Starlight/space-station-14
 
 using System.Text;
+using Content.Server.Antag;
 using Content.Server._Starlight.GameTicking.Rules.Components;
 using Content.Server.GameTicking.Rules;
 using Content.Server.Objectives;
+using Content.Server.Spawners.Components;
+using Content.Shared.Tag;
+using Robust.Shared.Prototypes;
 
 namespace Content.Server._Starlight.GameTicking.Rules;
 
@@ -20,11 +24,42 @@ namespace Content.Server._Starlight.GameTicking.Rules;
 /// </summary>
 public sealed partial class BrighteyeRuleSystem : GameRuleSystem<BrighteyeRuleComponent>
 {
+    private const string SpawnPointPrototype = "SpawnPointBrighteye";
+    private static readonly ProtoId<TagPrototype> TheDarkTag = "TheDark";
+
+    [Dependency] private TagSystem _tag = default!;
+    [Dependency] private SharedTransformSystem _transform = default!;
+
     public override void Initialize()
     {
         base.Initialize();
 
         SubscribeLocalEvent<BrighteyeRuleComponent, ObjectivesTextPrependEvent>(OnTextPrepend);
+        SubscribeLocalEvent<BrighteyeRuleComponent, AntagSelectLocationEvent>(OnSelectLocation);
+    }
+
+    /// <summary>
+    ///     O NestedRule da Whiskey carrega o mapa filho sem encaminhar os grids
+    ///     para o RuleGrids da regra pai. Resolve somente o ponto do Bright-eye,
+    ///     procurando seu marcador no mapa já identificado como TheDark.
+    /// </summary>
+    private void OnSelectLocation(Entity<BrighteyeRuleComponent> ent, ref AntagSelectLocationEvent args)
+    {
+        if (args.Handled)
+            return;
+
+        var query = EntityQueryEnumerator<SpawnPointComponent, TransformComponent>();
+        while (query.MoveNext(out var uid, out _, out var xform))
+        {
+            if (MetaData(uid).EntityPrototype?.ID != SpawnPointPrototype ||
+                xform.MapUid is not { } map ||
+                !_tag.HasTag(map, TheDarkTag))
+            {
+                continue;
+            }
+
+            args.Coordinates.Add(_transform.GetMapCoordinates(xform));
+        }
     }
 
     private void OnTextPrepend(EntityUid uid, BrighteyeRuleComponent comp, ref ObjectivesTextPrependEvent args)
